3fc28de454b70cb08a461ab323e31f31eb09ed98
Stephen Eaton <seaton@strobotics.com.au>
2016-07-09 22:16:12 +0800

    Initial Stub comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R-Programming Week 3 Assignment 2
+##  Stephen Eaton <seaton@strobotics.com.au>
+##
+## Two functions used to calculate the inverse of a matrix
 
-## Write a short comment describing this function
 
+## function: makeCacheMatrix
+##  Purpose:    Creates a matrix object that can cache its inverse
+## 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
-
+## function: cacheSolve
+##  Purpose:    Calculates the inverse of a matrix.
+##              If the inverse has already been calculated 
+##              (and the matrix has not changed), then cacheSolve will 
+##              retrieve the inverse from the cache.
+## 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
